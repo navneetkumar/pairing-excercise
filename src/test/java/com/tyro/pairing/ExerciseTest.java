db513a5dd65e66872f9c82eed9520affeb9c3b75
Stephen O'Rourke <sorourke@tyro.com>
2020-10-22 11:34:36 +1100

    Upgrade tests to junit 5 and bump all dependencies

--- src/test/java/com/tyro/pairing/ExerciseTest.java
+++ src/test/java/com/tyro/pairing/ExerciseTest.java
@@ -1,14 +1,14 @@
 package com.tyro.pairing;
 
+import org.junit.jupiter.api.Test;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-
-import org.junit.Test;
 
 public class ExerciseTest {
 
     @Test
-    public void shouldFail() throws Exception {
+    void shouldFail() {
         assertThat(true, is(false));
     }
 
--- src/test/kotlin/com/tyro/pairing/ExerciseKtTest.kt
+++ src/test/kotlin/com/tyro/pairing/ExerciseKtTest.kt
@@ -2,7 +2,7 @@ package com.tyro.pairing
 
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 class ExerciseKtTest {
 
